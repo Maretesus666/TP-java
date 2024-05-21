67efc1a79a3a4f0440189d00ba1e0797699f8d66
Maretesus666 <166169222+Maretesus666@users.noreply.github.com>
2024-05-21 17:20:03 -0300

    Add files via upload

---
+++ ej_1_java.java
@@ -0,0 +1,18 @@
+
+public class ej_1_java {
+
+	public static void main(String[] args) {
+
+for(int i=1; i<=100; i++)
+	{
+	if(i % 2 == 0 )
+	{
+		System.out.println("par");
+	}
+	else {
+	System.out.println(i);
+	
+	}
+	}
+}
+}
